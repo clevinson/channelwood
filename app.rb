a7daf69214a32de9a43a7e15d7c853a735992550
Cory Levinson <cory@soundcloud.com>
2014-04-03 22:01:52 +0100

    add basic auth, and basic bone structure

--- app.rb
+++ app.rb
@@ -1,8 +1,29 @@
 require 'sinatra'
 
+use Rack::Auth::Basic, "Restricted Area" do |username, password|
+    username == ENV['CHANNELWOOD_USERNAME'] and password == ENV['CHANNELWOOD_PASSWORD']
+end
+
 get '/' do
   erb :index
 end
+
+get '/splash' do
+  erb :splash
+end
+
+get '/home' do
+  erb :home
+end
+
+get '/physical/:cat_no' do
+  erb :physical, :locals => {:cat_no => params[:cat_no] }
+end
+
+get '/digital/:cat_no' do
+  "THIS IS THE DIGITAL PAGE 4 #{params['cat_no']}"  
+end
+  
 
 get '/buttweasel' do
   "matt is smelly"
