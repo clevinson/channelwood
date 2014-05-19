486976b5f9d955d73c83bb604c3e4c10c3a94bc2
Cory Levinson <cory@soundcloud.com>
2014-05-18 20:43:56 -0400

    first attempt at splash

---
+++ public/js/splash.js
@@ -0,0 +1,62 @@
+// Please note: dragging and dropping images only works for
+// certain browsers when serving this script online:
+
+var segLength = 12;
+var numLines = 100;
+var lineDepth = 10;
+var raster = null;
+var lines = new Array(numLines)
+var shapeSegments = []
+
+// As the web is asynchronous, we need to wait for the raster to
+// load before we can perform any operation on its pixels.
+new Raster('img/wip-logo.png').on('load', function() {
+  raster = this;
+  this.remove();
+  drawRect();
+});
+
+function onFrame(event) {
+  if(raster){
+    //var line = lines[Math.floor(Math.random()*numLines)];
+    for(var i=0; i < shapeSegments.length; i++){
+      var seg = shapeSegments[i];
+      seg.point.y = seg.point.y + 1*(Math.random() - 0.5);
+    }
+  }
+}
+
+function drawRect() {
+  raster.fitBounds(view.bounds);
+
+  for(var j=0; j < numLines; j++) {
+    lines[j] = new Path({
+      strokeColor: 'black',
+      closed: false
+    });
+
+    var y = raster.bounds.y + j*raster.bounds.height/numLines;
+    for(var i=0; i < raster.bounds.width/segLength; i++) {
+      var x = raster.bounds.x + i*segLength;
+
+      position = new Point(x,y);
+      if(raster.bounds.contains(position)){
+        var color = raster.getAverageColor(position);
+      }
+      var offset = color ? (1 - color.gray) * lineDepth : 0;
+      lines[j].add(new Point(x,y + offset));
+      if(color && (color.gray < 0.4)){
+        shapeSegments.push(lines[j].lastSegment);
+      }
+    }
+    lines[j].smooth();
+  }
+  console.log(shapeSegments);
+
+}
+
+
+function onResize() {
+  //if (raster)
+    //drawRect();
+}
