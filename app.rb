51929e9bab66c96ae865bfd54ba9a6074e997625
Cory Levinson <cory@soundcloud.com>
2016-08-19 00:42:22 +0200

    first pass at wip-003 digital

--- app.rb
+++ app.rb
@@ -159,6 +159,9 @@ class Channelwood < Sinatra::Base
       erb :wip_001, :locals => { :sc_client_id => ENV['SC_CLIENT_ID'] }
     elsif params[:cat_no].upcase == 'WIP-002'
       erb :wip_002, :locals => { :sc_client_id => ENV['SC_CLIENT_ID'] }
+    elsif params[:cat_no].upcase == 'WIP-003'
+      protected!
+      erb :wip_003, :locals => { :sc_client_id => ENV['SC_CLIENT_ID'] }
     else
       halt 404, '<h1>Not Found</h1>'
     end
